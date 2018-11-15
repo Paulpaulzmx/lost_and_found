4b725daecba0d539691e99ada67dde0ecbd64c10
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-11-15 14:27:15 +0800

    Update app/src/main/java/com/example/admin/helloworld/Listview/ListViewActivity.java

---
+++ app/src/main/java/com/example/admin/helloworld/Listview/ListViewActivity.java
@@ -0,0 +1,21 @@
+package com.example.admin.helloworld.Listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ListView;
+
+import com.example.admin.helloworld.R;
+
+public class ListViewActivity extends Activity {
+
+
+    private ListView mLv1;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview);
+        mLv1 = findViewById(R.id.Lv_1);
+        mLv1.setAdapter(new MyListAdapter(ListViewActivity.this));
+    }
+}
