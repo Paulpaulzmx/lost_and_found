dd9bd6c8bb71b6e1fa1eef4260e212ffad778951
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-11-26 17:29:15 +0800

    Update uiwuActivity.java

--- app/src/main/java/com/example/admin/helloworld/uiwuActivity.java
+++ app/src/main/java/com/example/admin/helloworld/uiwuActivity.java
@@ -1,43 +1,35 @@
-package com.example.admin.helloworld;
+﻿package com.example.admin.helloworld;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.bumptech.glide.Glide;
 import com.example.admin.helloworld.fragment.BFragment;
 import com.example.admin.helloworld.fragment.CFragment;
 import com.example.admin.helloworld.fragment.DFragment;
+import com.example.admin.helloworld.statusbar.StatusBarUtil;
+import com.example.admin.helloworld.util.GlideCircleTransform;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 
-public class uiwuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class uiwuActivity extends AppCompatActivity implements View.OnClickListener,NavigationView.OnNavigationItemSelectedListener {
+
 
 
     private TabLayout tabLayout;
@@ -45,6 +37,7 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
     private ImageView imageView;
     private ImageView imageView1;
     private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,23 +71,25 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
         tabLayout.setupWithViewPager(viewPager);
 
 
+
+
         eDt = findViewById(R.id.edt_search);
-        eDt.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(uiwuActivity.this, WebViewActivity.class);
+        eDt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(uiwuActivity.this,WebViewActivity.class);
                 startActivity(intent);
             }
         });
 
 
-        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
         imageView = findViewById(R.id.iv_title_menu1);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(uiwuActivity.this, ContainerActivity.class);
+        imageView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(uiwuActivity.this,ContainerActivity.class);
                 startActivity(intent);
             }
         });
@@ -110,43 +105,38 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
         });
 
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        StatusBarUtil.setColorForDrawerLayout(uiwuActivity.this, drawerLayout, 0, 0);
 
-        //点击发布按钮，出现对话框，选择拍照或是上传图片
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //navigationView.setNavigationItemSelectedListener(this);
+        navigationView.inflateHeaderView(R.layout.nav_header_drawer);
+        View headerView = navigationView.getHeaderView(0);
+        ImageView avatar = headerView.findViewById(R.id.iv_avatar);
+        headerView.findViewById(R.id.ll_nav_mess).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_scan_history).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_found).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_about).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_game).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_near).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_my).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_friends).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_collect).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_dark).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_set).setOnClickListener(this);
+        headerView.findViewById(R.id.ll_nav_exit).setOnClickListener(this);
+        Glide.with(getApplication()).load("http://47.106.146.182/pictures/profileicon603.jpg").transform(new GlideCircleTransform(avatar.getContext())).into(avatar);
+
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //新建下拉列表对话框
-                AlertDialog.Builder customizeDialog = new AlertDialog.Builder(uiwuActivity.this);
-                //自定义对话框样式，暂不需要
-//                final View dialogView = LayoutInflater.from(uiwuActivity.this)
-//                        .inflate(R.layout.dialog_customize, null);
-//                customizeDialog.setTitle("");
-//                customizeDialog.setView(dialogView);
-                final String[] items = {"拍照", "上传本地图片"};      //列表项
-                customizeDialog.setItems(items, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0:
-                                Intent takePictureIntent = new Intent(
-                                        uiwuActivity.this,TakePhotoActivity.class);
-                                startActivity(takePictureIntent);
-                                break;
-                            case 1:
-                                //选择本地图片
-                                Intent localPictureIntent = new Intent(
-                                        uiwuActivity.this, SelectActivity.class);
-                                startActivity(localPictureIntent);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                });
-                customizeDialog.show(); //显示对话框
+                Intent intent = new Intent(uiwuActivity.this,EditTextActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -155,7 +145,7 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // 绑定toolbar跟menu
+        // 绑定toobar跟menu
         getMenuInflater().inflate(R.menu.rightview_menu, menu);
         return true;
     }
@@ -168,22 +158,24 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch (id) {
+        switch (id){
             case R.id.actionbar_wode:
-                Toast.makeText(getApplicationContext(), "Toast我的发布", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Toast我的发布",Toast.LENGTH_LONG).show();
                 break;
             case R.id.actionbar_jmyi:
-                Toast.makeText(getApplicationContext(), "Toast建议", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Toast建议",Toast.LENGTH_LONG).show();
                 break;
             case R.id.actionbar_more:
-                Toast.makeText(getApplicationContext(), "Toastgg", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Toastgg",Toast.LENGTH_LONG).show();
                 break;
         }
+
+
         return super.onOptionsItemSelected(item);
     }
 
     public void onBackPressed() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
@@ -191,29 +183,24 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        switch (id) {
-            case R.id.pavl:
-                Toast.makeText(getApplicationContext(), "拍照", Toast.LENGTH_LONG).show();
+        switch (id){
+            case R.id.pavl:Toast.makeText(getApplicationContext(),"拍照",Toast.LENGTH_LONG).show();
                 break;
-            case R.id.liui:
-                Toast.makeText(getApplicationContext(), "历史", Toast.LENGTH_LONG).show();
+            case R.id.liui:Toast.makeText(getApplicationContext(),"历史",Toast.LENGTH_LONG).show();
                 break;
-            case R.id.uip:
-                Toast.makeText(getApplicationContext(), "等带替换1", Toast.LENGTH_LONG).show();
+            case R.id.uip:Toast.makeText(getApplicationContext(),"等带替换1",Toast.LENGTH_LONG).show();
                 break;
-            case R.id.uevi:
-                Toast.makeText(getApplicationContext(), "等待替换2", Toast.LENGTH_LONG).show();
+            case R.id.uevi:Toast.makeText(getApplicationContext(),"等待替换2",Toast.LENGTH_LONG).show();
                 break;
-            case R.id.faxm:
-                Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_LONG).show();
+            case R.id.faxm:Toast.makeText(getApplicationContext(),"3",Toast.LENGTH_LONG).show();
                 break;
-            case R.id.gryu:
-                Toast.makeText(getApplicationContext(), "4", Toast.LENGTH_LONG).show();
+            case R.id.gryu:Toast.makeText(getApplicationContext(),"4",Toast.LENGTH_LONG).show();
                 break;
         }
 
@@ -224,12 +211,20 @@ public class uiwuActivity extends AppCompatActivity implements NavigationView.On
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.ll_title_menu:
-                // 开启菜单
-                drawerLayout.openDrawer(GravityCompat.START);
+            case R.id.ll_nav_mess:
+                Intent intent = new Intent(uiwuActivity.this,Main2Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.ll_nav_exit:
+                // 退出应用
+                finish();
+                break;
 
             default:
                 break;
         }
     }
+
+
+
 }
