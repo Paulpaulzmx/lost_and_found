eff03973bff3de62e8f22e54b534546a51e9af18
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-12-14 10:15:40 +0800

    Create AFragment.java

---
+++ app/src/main/java/com/example/admin/helloworld/fragment/AFragment.java
@@ -0,0 +1,34 @@
+package com.example.admin.helloworld.fragment;
+
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.example.admin.helloworld.R;
+
+public class AFragment extends Fragment {
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_a,container,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
