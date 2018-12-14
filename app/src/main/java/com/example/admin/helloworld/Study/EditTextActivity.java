db29962c65b7878192fb8b6336f0da91cb289e2c
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-12-14 10:18:31 +0800

    Create EditTextActivity.java

---
+++ app/src/main/java/com/example/admin/helloworld/Study/EditTextActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.helloworld.Study;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.helloworld.R;
+
+public class EditTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+    }
+}
