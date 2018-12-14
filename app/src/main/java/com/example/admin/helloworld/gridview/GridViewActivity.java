eb304bba8d844011e438df8a0ba074b7ce424590
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-12-14 10:16:27 +0800

    Create GridViewActivity.java

---
+++ app/src/main/java/com/example/admin/helloworld/gridview/GridViewActivity.java
@@ -0,0 +1,20 @@
+package com.example.admin.helloworld.gridview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import com.example.admin.helloworld.R;
+
+public class GridViewActivity extends AppCompatActivity {
+
+    private GridView mGv;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridview);
+        mGv = findViewById(R.id.gv);
+        mGv.setAdapter(new MyGridViewAdapter(GridViewActivity.this));
+    }
+}
