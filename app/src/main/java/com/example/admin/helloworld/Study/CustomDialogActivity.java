afd7d9cde239fe96945989661291c74d79363d2a
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-12-14 10:18:21 +0800

    Create CustomDialogActivity.java

---
+++ app/src/main/java/com/example/admin/helloworld/Study/CustomDialogActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.helloworld.Study;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.helloworld.R;
+
+public class CustomDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_dialog);
+    }
+}
