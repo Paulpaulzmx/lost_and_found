a2d6db376cbd115bbdb8b1c073e453b3b414b710
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-12-14 10:19:33 +0800

    Create WebViewActivity.java

---
+++ app/src/main/java/com/example/admin/helloworld/WebViewActivity.java
@@ -0,0 +1,74 @@
+package com.example.admin.helloworld;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+
+    private WebView mWvMain;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        mWvMain = findViewById(R.id.wv);
+        //mWvMain.loadUrl("file:///android_asset/index02.html");
+        mWvMain.getSettings().setJavaScriptEnabled(true);
+        mWvMain.setWebViewClient(new MyWebViewClient());
+        mWvMain.setWebChromeClient(new MyWebChromeClient());
+        mWvMain.loadUrl("https://m.baidu.com");
+
+    }
+
+    class MyWebChromeClient extends WebChromeClient{
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            setTitle(title);
+        }
+    }
+
+
+    class MyWebViewClient extends WebViewClient{
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            Log.d("WebView","onPageStarted...");
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            Log.d("WebView","onPageFinished...");
+            mWvMain.loadUrl("javascript:alert('hello')");
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && mWvMain.canGoBack()){
+            mWvMain.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
