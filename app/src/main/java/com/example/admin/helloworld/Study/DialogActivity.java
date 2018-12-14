2355c54d81c2298fc2383c76009424848b833ddf
Rosetime <37096345+Rosetime@users.noreply.github.com>
2018-12-14 10:18:26 +0800

    Create DialogActivity.java

---
+++ app/src/main/java/com/example/admin/helloworld/Study/DialogActivity.java
@@ -0,0 +1,71 @@
+package com.example.admin.helloworld.Study;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.admin.helloworld.R;
+
+public class DialogActivity extends AppCompatActivity {
+
+    private Button mBtnDialog1,mBtnDialog2,mBtnDialog3,mBtnDialog4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+        mBtnDialog1 = findViewById(R.id.btn_dialog1);
+        mBtnDialog2 = findViewById(R.id.btn_dialog2);
+        mBtnDialog3 = findViewById(R.id.btn_dialog3);
+        mBtnDialog4 = findViewById(R.id.btn_dialog4);
+        OnClick onClick = new OnClick();
+
+        mBtnDialog1.setOnClickListener(onClick);
+        mBtnDialog2.setOnClickListener(onClick);
+        mBtnDialog3.setOnClickListener(onClick);
+        mBtnDialog4.setOnClickListener(onClick);
+
+
+
+
+    }
+
+    class OnClick implements View.OnClickListener{
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.btn_dialog1:
+                    AlertDialog.Builder builer = new AlertDialog.Builder(DialogActivity.this);
+                    builer.setTitle("请回答").setMessage("你觉得怎么样啊？？？")
+                    .setPositiveButton("可以", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(DialogActivity.this, "你很真的", Toast.LENGTH_SHORT).show();
+                        }
+                    }).setNeutralButton("还行", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Toast.makeText(DialogActivity.this, "假的", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }).setNegativeButton("不好", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(DialogActivity.this, "what the fuck", Toast.LENGTH_SHORT).show();
+                        }
+                    }).show();
+                    break;
+                case R.id.btn_dialog2:
+                    break;
+                case R.id.btn_dialog3:
+                    break;
+                case R.id.btn_dialog4:
+                    break;
+            }
+        }
+    }
+}
